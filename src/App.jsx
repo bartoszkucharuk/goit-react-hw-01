fd61fe285473def356f4a45783ebbdbe6874ef39
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-08-21 18:27:11 +0200

    hurra

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(10)
 
   return (
     <>
@@ -28,8 +29,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Profile />
     </>
   )
 }
+
+
 
 export default App
---
+++ src/components/Profile.jsx
@@ -0,0 +1,9 @@
+
+function Profile() {
+    return (
+        <div className="profile_div">
+            <p> Cześć!</p>
+        </div>
+    )
+}
+ export default Profile
