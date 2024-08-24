b2f4a9fbffdee088e6f989972af5f5187ca9f3ff
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-08-24 10:30:46 +0200

    <Profile />

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import Profile from './components/Profile.jsx'
+
+import Profile from './components/Profile.jsx';
+import userData from "./components/userData.json";
 
 function App() {
   return (
-    <Profile />
-  )
+    <>
+      
+      <Profile
+      
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+
+    </>
+    )
 }
 export default App
+ 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,32 @@
+import userData from "./userData.json"
 
 function Profile() {
     return (
-        <div className="profile">
-            <p> Cześć!</p>
+        <div>
+            <div>
+                <img
+                src="<https://cdn-icons-png.flaticon.com/512/1077/1077012.png>"
+                alt="User avatar"
+                />
+                <p>{userData.username}</p>
+                <p>@{userData.tag}</p>
+                <p>{userData.location}</p>
+            </div>
+
+            <ul>
+                <li>
+                    <span>Followers</span>
+                    <span>{userData.stats.followers}</span>
+                </li>
+                <li>
+                    <span>Views</span>
+                    <span>{userData.stats.views}</span>
+                </li>
+                <li>
+                    <span>Likes</span>
+                    <span>{userData.stats.likes}</span>
+                </li>
+            </ul>
         </div>
     );
 }
