276132c94c87d8e37ad8f6cbc2edea12a48adbdf
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-08-25 21:10:45 +0200

    3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Profile from './components/Profile/Profile.jsx';
 import user from "./components/Profile/userData.json";
 import FriendList from './components/FriendList/FriendList.jsx'
 import friends from "./components/FriendList/friends.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory.jsx"
+import transactions from "./components/TransactionHistory/transactions.json"
 
 function App() {
   return (
@@ -12,11 +14,9 @@ function App() {
         tag={user.tag}
         location={user.location}
         image={user.avatar}
-        stats={user.stats}
-      />
-
+        stats={user.stats}/>
       <FriendList friends={friends} />
-
+      <TransactionHistory items={transactions} />
     </div>
     )
 }
